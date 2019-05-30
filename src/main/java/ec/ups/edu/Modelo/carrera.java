6b48f405abf66682c18d83bdb5bff31bc29ee157
chrispicon914 <chrispicon914@gmail.com>
2019-05-29 23:47:49 -0500

    Add files via upload

---
+++ src/main/java/ec/ups/edu/Modelo/carrera.java
@@ -0,0 +1,65 @@
+package ec.ups.edu.Modelo;
+
+public class carrera {
+	
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private String titulo;
+	private String modalidad;
+	private String duracion;
+	
+	public carrera() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getModalidad() {
+		return modalidad;
+	}
+
+	public void setModalidad(String modalidad) {
+		this.modalidad = modalidad;
+	}
+
+	public String getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+	
+
+}
---
+++ src/main/java/ec/ups/edu/Modelo/persona.java
@@ -0,0 +1,83 @@
+package ec.ups.edu.Modelo;
+
+public class persona {
+	
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private int edad;
+	private String usuario;
+	private String contraseña;
+	private String telefono;
+	
+	public persona () {
+		
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+
+}
